24b506a114f254635e6de34caaa09825ae681984
bijon2002 <bijosilin02@gmail.com>
2025-12-30 18:18:47 +0530

    cart page Linked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import ProductDetail from './pages/productDetail';
 import { useState } from 'react';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Cart from './pages/Cart';
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Home />} />
           <Route path="/product/:id" element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems} />} />
+           <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
 
          
 
